ae7e4f4fc7de3397a4c0eebfb4577959d4832f55
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-07-27 12:59:00 +0100

    chore: Footer UI done

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from '@chakra-ui/react';
+
+const Footer = () => {
+  return (
+    <Center>
+      <Text size={'sm'}>AzeezDesigns @copyright</Text>
+    </Center>
+  );
+};
+
+export default Footer;
