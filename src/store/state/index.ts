c1330018156fc046594229a6c9bfc88372ab04bd
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-07-27 13:01:59 +0100

    chore: initial State

--- src/store/state/index.ts
+++ src/store/state/index.ts
@@ -1,4 +1,26 @@
-export interface initialState {
-  user: any;
+export interface IUser {
+  code: number;
+  message: string;
+  module: string;
+  results: {
+    by_default: string;
+    company_id: string;
+    icons: string;
+    is_first: number;
+    status: string;
+    token: string;
+    user_id: string;
+    want_login: string;
+  };
+  status: string;
+}
+
+export interface IState {
+  user: IUser | null;
   isAuthenticated: false;
 }
+
+export const initialState: IState = {
+  user: null,
+  isAuthenticated: false,
+};
