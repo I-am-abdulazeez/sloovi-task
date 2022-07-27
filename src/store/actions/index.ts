dc57bc8694db3ee15d3a9c366ef17b18c3e3b258
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-07-27 09:56:07 +0100

    chore: created Auth actions: Login and Logout for user

---
+++ src/store/actions/index.ts
@@ -0,0 +1,14 @@
+import { AuthActionTypes } from '../types';
+
+interface LOGINAction {
+  type: AuthActionTypes.LOGIN;
+  payload: {
+    email: string;
+    password: string;
+  };
+}
+
+interface LOGOUTAction {
+  type: AuthActionTypes.LOGOUT;
+  payload: {};
+}
