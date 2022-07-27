98bb365422e2fc641fb4f1b23b3b91b77ade97f2
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-07-27 12:36:43 +0100

    chore: navbar component composition

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,35 @@
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Container,
+  Heading,
+  HStack,
+  IconButton,
+  Spacer,
+  Text,
+  useColorMode,
+} from '@chakra-ui/react';
+
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const icon = colorMode === 'light' ? <SunIcon /> : <MoonIcon />;
+  return (
+    <Box height={'3vh'} py={1}>
+      <Container maxWidth={'container.md'}>
+        <HStack>
+          <Heading size={'md'}>Sloov</Heading>
+          <Spacer />
+          <IconButton
+            aria-label="Toggle dark mode"
+            icon={icon}
+            size={'sm'}
+            onClick={toggleColorMode}
+            variant="ghost"
+          />
+        </HStack>
+      </Container>
+    </Box>
+  );
+};
+
+export default Navbar;
