7a405a75e6d7ab7eb92306b5cfc92adc6d1ebef4
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-07-27 13:02:14 +0100

    chore: User interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,25 @@ export interface Admin {
   email: string;
   password: string;
 }
+
+export interface IUser {
+  code: number;
+  message: string;
+  module: string;
+  results: {
+    by_default: string;
+    company_id: string;
+    icons: string;
+    is_first: number;
+    status: string;
+    token: string;
+    user_id: string;
+    want_login: string;
+  };
+  status: string;
+}
+
+export interface IState {
+  user: IUser | null;
+  isAuthenticated: false;
+}
