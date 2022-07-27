bc80ce564e17ad40d8c244f8dfae1227cbd95f77
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-07-27 11:23:44 +0100

    chore: created LoginUser action

--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -1,16 +1,19 @@
+import { Dispatch } from 'redux';
+import AppService from '../../services';
+import { AuthAction } from '../actionsTypes';
 import { AuthActionTypes } from '../types';
 
-interface LOGINAction {
-  type: AuthActionTypes.LOGIN;
-  payload: {
-    email: string;
-    password: string;
+export const LoginUser =
+  (user: { email: string; password: string }) =>
+  async (dispatch: Dispatch<AuthAction>) => {
+    try {
+      const res = await AppService.loginUser(user);
+      dispatch({
+        type: AuthActionTypes.LOGIN,
+        payload: res.data,
+      });
+      return Promise.resolve(res.data);
+    } catch (err) {
+      return Promise.reject(err);
+    }
   };
-}
-
-interface LOGOUTAction {
-  type: AuthActionTypes.LOGOUT;
-  payload: {};
-}
-
-export type AuthAction = LOGINAction | LOGOUTAction;
