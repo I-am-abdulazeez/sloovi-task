c42fcc3f44af1ac5d173369cfe181d3e48912310
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-07-27 13:01:04 +0100

    chore: Authreducer function

--- src/store/reducers/AuthReducer.ts
+++ src/store/reducers/AuthReducer.ts
@@ -2,7 +2,7 @@ import { AuthAction } from '../actionsTypes';
 import { initialState } from '../state';
 import { AuthActionTypes } from '../types';
 
-export const AuthReducer = (state: initialState, action: AuthAction) => {
+export const AuthReducer = (state = initialState, action: AuthAction) => {
   switch (action.type) {
     case AuthActionTypes.LOGIN:
       return {
@@ -13,7 +13,7 @@ export const AuthReducer = (state: initialState, action: AuthAction) => {
     case AuthActionTypes.LOGOUT:
       return {
         ...state,
-        isAuthenticated: false,
+        user: action.payload,
       };
     default:
       return state;
