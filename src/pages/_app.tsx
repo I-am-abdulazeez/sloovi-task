c2f3794e7b7897228231432e3375ec0cf5f81504
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-07-27 12:36:27 +0100

    chore: Added Navbar and Footer components respectively

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,22 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
 import { Provider } from 'react-redux';
+
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+
 import { store } from '../store';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
       <ChakraProvider>
+        <Navbar />
         <Component {...pageProps} />
+        <Footer />
       </ChakraProvider>
     </Provider>
   );
-}
+};
 
 export default MyApp;
