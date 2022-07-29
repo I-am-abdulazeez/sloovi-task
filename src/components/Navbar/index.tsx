87d28112dac6b83ad4bb523195ed200e9e5a2f5b
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-07-29 11:25:15 +0100

    chore: Use throughout your app instead of plain `useDispatch` and `useSelector`

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,31 +1,55 @@
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import {
   Box,
+  Button,
   Container,
   Heading,
   HStack,
   IconButton,
   Spacer,
-  Text,
   useColorMode,
 } from '@chakra-ui/react';
+import { logoutUser } from 'src/features/Auth/auth-slice';
+
+import { useAppDispatch, useAppSelector } from 'src/store/hooks';
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
-  const icon = colorMode === 'light' ? <SunIcon /> : <MoonIcon />;
+
+  const storeState = useAppSelector((state) => state.auth);
+  const storeDispatch = useAppDispatch();
+
+  const handleUserLogout = () => {
+    storeDispatch(logoutUser());
+  };
+
+  console.log(storeState);
+
   return (
     <Box py={1}>
       <Container maxWidth={'container.md'}>
         <HStack>
           <Heading size={'md'}>Sloov</Heading>
           <Spacer />
-          <IconButton
-            aria-label="Toggle dark mode"
-            icon={icon}
-            size={'sm'}
-            onClick={toggleColorMode}
-            variant="ghost"
-          />
+          <Box>
+            <IconButton
+              aria-label="Toggle dark mode"
+              icon={colorMode === 'light' ? <SunIcon /> : <MoonIcon />}
+              size={'sm'}
+              mx={4}
+              onClick={toggleColorMode}
+              variant="ghost"
+            />
+            {storeState.isAuthenticated && (
+              <Button
+                onClick={handleUserLogout}
+                size={'sm'}
+                colorScheme={'red'}
+              >
+                Logout
+              </Button>
+            )}
+          </Box>
         </HStack>
       </Container>
     </Box>
