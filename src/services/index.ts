c07c9542c688079522036bf8fdfc00a000a6fbeb
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-07-29 11:20:59 +0100

    chore: removed services to use slices

--- src/services/index.ts
+++
@@ -1,9 +0,0 @@
-import http from '../http';
-
-class AppService {
-  loginUser = (user: { email: string; password: string }) => {
-    return http.post('/login', user);
-  };
-}
-
-export default new AppService();
