f93c9970f743ba502d9379821c54d517ab33f2aa
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-07-29 11:22:23 +0100

    chore: Login mockup with forms and hooks t manage formState, then store hooks to use store values

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import Head from 'next/head';
+
+import { Input, Center, VStack, Heading, Button } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+import { Admin } from '../models';
 
 const Login = () => {
-  return <div>login</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<Admin>();
+
+  const handleAdminLogin: SubmitHandler<Admin> = (data) => {
+    const admin = { email: data.email, password: data.password };
+    console.log(admin);
+  };
+
+  return (
+    <Center height={'89vh'}>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <VStack spacing={5}>
+        <Heading size={'md'}>Admin Login</Heading>
+        <form onSubmit={handleSubmit(handleAdminLogin)}>
+          <VStack spacing={4}>
+            <Input
+              width={'300px'}
+              placeholder="Email"
+              {...register('email', {
+                required: true,
+              })}
+            />
+            <Input
+              width={'300px'}
+              placeholder="Password"
+              {...register('password', {
+                required: true,
+              })}
+            />
+            <Button
+              colorScheme={'purple'}
+              type="submit"
+              isLoading={isSubmitting}
+              size={'sm'}
+              width={'100%'}
+            >
+              Login Admin
+            </Button>
+          </VStack>
+        </form>
+      </VStack>
+    </Center>
+  );
 };
 
 export default Login;
