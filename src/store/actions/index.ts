9dbe340971fede6b6e95cc7a5ba6b8160519bc8a
AbdulAzeez <abdulazeezolamide50@gmail.com>
2022-07-31 06:10:48 +0100

    refactor: using src/

--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -1,9 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import Router from 'next/router';
 
-import { Dispatch } from 'redux';
-import http from '../../http';
-import { Admin } from '../../models';
+import http from 'src/http';
+import type { Admin } from 'src/models';
 
 export const LoginUser = createAsyncThunk('user/fetchUsers', (data: Admin) => {
   return http.post('/login', data).then((response) => response.data);
